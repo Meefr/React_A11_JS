d4488e2188314317ce8f1f23dbefc3ca21add976
Meefr <mohamedelramah737@gmail.com>
2024-07-20 16:48:58 +0300

    project almost done but api just simulate the POST/PUT methods but without update the server

--- APIs/Apis.js
+++ APIs/Apis.js
@@ -1,6 +1,11 @@
 const base_url = "https://dummyjson.com/";
 
-export async function handelRemoteRequest(endpoint, success, faild,lodingTrigger) {
+export async function handelRemoteRequest(
+  endpoint,
+  success,
+  faild,
+  lodingTrigger
+) {
   // handle sucess
   try {
     lodingTrigger("start");
@@ -14,12 +19,39 @@ export async function handelRemoteRequest(endpoint, success, faild,lodingTrigger
     }
   } catch (e) {
     faild(e.message);
+    // console.log(e.message);
+  } finally {
+    lodingTrigger("end");
+    // console.log("fetching data is done");
+  }
+}
+export async function handelRemotePost(
+  endpoint,
+  faild,
+  lodingTrigger,
+  data,
+  method,
+  merged
+) {
+  try {
+    const response = await fetch(`${base_url}${endpoint}`, {
+      method: `${method}`,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+      merge: merged,
+    });
+    if (true) {
+      const data = await response.json();
+      // success(data);
+      // return data;
+    } else {
+      throw new Error("something went wrong in fetching data!");
+    }
+  } catch (e) {
+    faild(e.message);
     console.log(e.message);
   } finally {
     lodingTrigger("end");
     console.log("fetching data is done");
   }
 }
-
-
-
--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,6 @@
-import { handelRemoteRequest } from "..//APIs/Apis.js";
+import { handelRemoteRequest, handelRemotePost } from "..//APIs/Apis.js";
 // get categories
 const cat = $("#side-bar");
-console.log(cat);
-const api_status = {
-  loading: true,
-  error: null,
-};
 const categoriesContainer = $("#categories");
 const loadingDoc = $("#loading");
 const contentDoc = $("#content-section");
@@ -16,24 +11,68 @@ const cartContainerCloseBtn = $("#cart-close-btn");
 const cartIcon = $("#cart-icon");
 const cartLength = $("#cart-number");
 const cartItemsContainer = $("#cart-items-container");
+const cartNumber = 1;
 let cartItems = [];
+let isChanges = 0;
 
 // function to get data from endpoint
 // handel ui based on response state
 // loding , pending , finish
 
+window.addEventListener("beforeunload", () => {
+  if (isChanges != 0) {
+    console.log("before loading!");
+    handelRemotePost(
+      `carts/${cartNumber}`,
+      faild,
+      lodingTrigger,
+      cartItems,
+      "PUT",
+      true
+    );
+  }
+});
+
 function addTocart(product) {
+  isChanges++;
   const productIndex = cartItems.findIndex((item) => item.id === product.id);
   if (productIndex !== -1) {
-    // Product exists, increase quantity by 1
-    cartItems[productIndex].quantity += 1;
-    // Update the total price for the product
+    cartItems[productIndex].quantity =
+      Number(cartItems[productIndex].quantity) + 1;
     cartItems[productIndex].total =
-      cartItems[productIndex].quantity * cartItems[productIndex].price;
+      cartItems[productIndex].quantity * Number(cartItems[productIndex].price);
   } else {
-    // Product doesn't exist, push the product into cartItems
     cartItems.push(product);
   }
+  successCarts({ products: cartItems });
+}
+function addOneItem(id) {
+  isChanges++;
+  const productIndex = cartItems.findIndex((item) => item.id === id);
+  cartItems[productIndex].quantity =
+    Number(cartItems[productIndex].quantity) + 1;
+  cartItems[productIndex].total =
+    cartItems[productIndex].quantity * Number(cartItems[productIndex].price);
+
+  successCarts({ products: cartItems });
+}
+function removeOneItem(id) {
+  isChanges--;
+  const productIndex = cartItems.findIndex((item) => item.id === id);
+  cartItems[productIndex].quantity =
+    Number(cartItems[productIndex].quantity) - 1;
+  cartItems[productIndex].total =
+    cartItems[productIndex].quantity * Number(cartItems[productIndex].price);
+  if (cartItems[productIndex].quantity <= 0) {
+    cartItems.splice(productIndex, 1);
+  }
+  successCarts({ products: cartItems });
+}
+
+function removeFromCart(productId) {
+  isChange -= cartItems[productId].quantity;
+  cartItems = cartItems.filter((item) => item.id !== productId);
+  successCarts({ products: cartItems });
 }
 
 function lodingTrigger(trigger) {
@@ -82,18 +121,28 @@ function successCats(data) {
       );
     });
   });
-  console.log(data);
 }
 
 function successProducts(data) {
   console.log(data);
   console.log(productsDoc);
   productsDoc.html(
-    data.products.map(
-      (product, index) =>
-        `          <div class="col-4">
+    data.products
+      .map((product, index) => {
+        let pCart = {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          quantity: 1,
+          total: product.price,
+          discountPercentage: product.discountPercentage,
+          discountedTotal: product.discountPercentage,
+          thumbnail: product.thumbnail,
+        };
+        return `
+          <div class="col-4">
             <div class="border rounded-2">
-              <img src="${product.thumbnail}"class="w-100 mb-2" alt="">
+              <img src="${product.thumbnail}" class="w-100 mb-2" alt="">
               <div class="px-2">
                 <div class="">
                   <h3>${product.title}</h3>
@@ -101,33 +150,37 @@ function successProducts(data) {
                 </div>
                 <div class="d-flex flex-nowrap align-items-center gap-1 px-3 py-2">
                   <i class="fa fa-star" style="color:#F7C600"></i>
-                  <p class="m-0 bg-danger bg-opacity-50 p-1 rounded-2">${product.rating}</p>
+                  <p class="m-0 bg-danger bg-opacity-50 p-1 rounded-2">${
+                    product.rating
+                  }</p>
                 </div>
-                <div class="d-flex justify-content-around align-items-center flex-nowrap gap-1  py-2">
+                <div class="d-flex justify-content-around align-items-center flex-nowrap gap-1 py-2">
                   <p class="m-0 fw-bold fs-3">$${product.price}</p>
-                  <button type="button" class="btn btn-danger">Add to cart</button>
+                  <button type="button" class="btn btn-danger add-to-cart-btn" data-product='${JSON.stringify(
+                    pCart
+                  )}'>Add to cart</button>
                 </div>
               </div>
             </div>
-          </div>`
-    )
+          </div>`;
+      })
+      .join("")
   );
-}
 
-function removeFromCart(productId) {
-  cartItems = cartItems.filter((item) => item.id !== productId);
-  successCarts({ products: cartItems });
+  $(".add-to-cart-btn").on("click", function () {
+    const product = JSON.parse($(this).attr("data-product"));
+    addTocart(product);
+  });
 }
-
 function successCarts(data) {
-  cartItems = data.products; 
+  cartItems = data.products;
   cartLength.text(data.products.length);
   cartItemsContainer.html(
     data.products
       .map((item) => {
         return `<div class="d-flex justify-content-around align-items-center gap-2 position-relative p-4">
                 <div class="position-absolute top-0 start-0 bg-danger d-flex justify-content-center align-items-center"
-                  style="width: 30px; height: 30px;" onclick="removeFromCart(${item.id})">
+                  style="width: 30px; height: 30px; cursor:pointer;" id = "${item.id}">
                   <i class="fa-solid fa-xmark"></i>
                 </div>
                 <div class="d-flex flex-column gap-2 justify-content-center align-items-center flex-wrap">
@@ -135,18 +188,26 @@ function successCarts(data) {
                   <p class="m-0">Price: <span>${item.quantity}</span> x <span>${item.price}</span> : <span>${item.total}</span></p>
                 </div>
                 <div class="d-flex flex-row flex-md-column gap-3 justify-content-center align-items-center flex-wrap">
-                  <i class="fa-solid fa-up-long"></i>
-                  <i class="fa-solid fa-down-long"></i>
+                  <i class="fa-solid fa-up-long" id="add-item-${item.id}" style="cursor:pointer;"></i>
+                  <i class="fa-solid fa-down-long" id="delete-item-${item.id}" style="cursor:pointer;"></i>
                 </div>
               </div>`;
       })
       .join("")
   );
-  let x = $("#144")
-    console.log("X",x);
+  data.products.forEach((item) => {
+    $(`#${item.id}`).on("click", () => {
+      removeFromCart(item.id);
+    });
+    $(`#add-item-${item.id}`).on("click", () => {
+      addOneItem(item.id);
+    });
+    $(`#delete-item-${item.id}`).on("click", () => {
+      removeOneItem(item.id);
+    });
+  });
 }
 
-removeFromCart(169)
 // handelRemoteRequest(
 //   "products/categories",
 //   successCats,
@@ -163,7 +224,12 @@ handelRemoteRequest("products", successProducts, faild, lodingTrigger)
     )
   )
   .then(() =>
-    handelRemoteRequest("carts/1", successCarts, faild, lodingTrigger)
+    handelRemoteRequest(
+      `carts/${cartNumber}`,
+      successCarts,
+      faild,
+      lodingTrigger
+    )
   );
 
 //loding
@@ -176,4 +242,3 @@ cartContainerCloseBtn.on("click", () => {
   cartContainer.removeClass("end-0");
   cartContainer.addClass("out-left-screen");
 });
-
